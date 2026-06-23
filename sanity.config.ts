4af5677d2f93c3bd52c57674003cb7211caada82
fikriarkan1309 <fikriarkan1309@gmail.com>
2026-06-23 21:50:44 +0700

    Update default project ID in sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@ import { structureTool } from 'sanity/structure';
 import { schemaTypes } from './schemas';
 
 // Fallback is helpfully structured to support environment variables during local or cloud builds
-const projectId = process.env.SANITY_STUDIO_PROJECT_ID || process.env.VITE_SANITY_PROJECT_ID || 'your-sanity-project-id';
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID || process.env.VITE_SANITY_PROJECT_ID || '894fyinb';
 const dataset = process.env.SANITY_STUDIO_DATASET || process.env.VITE_SANITY_DATASET || 'production';
 
 export default defineConfig({
